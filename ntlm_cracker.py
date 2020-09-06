aaf8dd3308adba121cc3e2f2f39539efc62ca093
Azan Shahid <67824275+azan121468@users.noreply.github.com>
2020-09-06 07:42:21 +0200

    Add files via upload

---
+++ ntlm_cracker.py
@@ -0,0 +1,45 @@
+import binascii
+import hashlib
+
+
+def title_box():
+    """Creates title box"""
+    print("            **********************")
+    print("            *     MD5 Cracker    *")
+    print("            *        by          *")
+    print("            *     Azan Shahid    *")
+    print("            **********************")
+
+def ntlm_hash(string):
+    """generate ntlm hash"""
+    return binascii.hexlify(hashlib.new('md4', string.encode('utf-16le')).digest()).decode('utf-8')
+
+def crack(hash, wordlist):
+    """crack ntlm hash"""
+    try:
+        with open(wordlist) as wordlist:
+            a = wordlist.readlines()
+            for each in a:
+                if each.endswith('\n'):
+                    word = each.strip()  # remove space and \n from start and end
+                else:
+                    word = each
+                print("Trying ---> ", word)
+                if ntlm_hash(word) == hash:
+                    print("Found  ---> ", word)
+                    break
+    except IOError:
+        print("I/O error occured")
+        print("Cannot open wordlist")
+
+def main():
+    """main part of program"""
+    title_box()
+    print("Enter your NTLM Hash : ",end="")
+    user_input = input() or "admin"
+    print("Enter your wordlist : ",end="")
+    wordlist = input() or "wordlist.txt"
+    crack(user_input, wordlist)
+    
+if __name__=="__main__":
+    main()
