e257ce8c45f906eea044025ff9f506ca5fd8ca6d
Azan Shahid <67824275+azan121468@users.noreply.github.com>
2020-09-14 09:55:34 -0700

    Update ntlm_cracker.py

--- ntlm_cracker.py
+++ ntlm_cracker.py
@@ -2,21 +2,23 @@ import binascii
 import hashlib
 from time import time
 
+no_of_try = 0
 
 def title_box():
     """Creates title box"""
-    print("            ***********************")
-    print("            *     NTLM Cracker    *")
-    print("            *         by          *")
-    print("            *     Azan Shahid     *")
-    print("            ***********************")
+    print("            **********************")
+    print("            *     MD5 Cracker    *")
+    print("            *        by          *")
+    print("            *     Azan Shahid    *")
+    print("            **********************")
 
 def ntlm_hash(string):
     """generate ntlm hash"""
     return binascii.hexlify(hashlib.new('md4', string.encode('utf-16le')).digest()).decode('utf-8')
 
-def crack(hash, wordlist):
+def crack(user_hash, wordlist):
     """crack ntlm hash"""
+    global no_of_try
     try:
         with open(wordlist) as wordlist:
             a = wordlist.readlines()
@@ -25,13 +27,15 @@ def crack(hash, wordlist):
                     word = each.strip()  # remove space and \n from start and end
                 else:
                     word = each
-                print("Trying ---> ", word)
-                if ntlm_hash(word) == hash:
+                print("Trying ---> "+ word)
+                no_of_try+=1
+                if ntlm_hash(word) == user_hash.lower():
                     print("Found  ---> ", word)
                     break
     except IOError:
         print("I/O error occured")
         print("Cannot open wordlist")
+        sys.exit()
 
 def main():
     """main part of program"""
@@ -43,7 +47,8 @@ def main():
     start = time()
     crack(user_input, wordlist)
     stop = time()
-    print(f"Time taken is {int(float(stop-start))}")
+    print(f"Time taken is {round(float(stop-start),3)} seconds")
+    print(f"Average speed was {round(no_of_try/(stop-start),2)} passwords per seconds")
 
 if __name__=="__main__":
     main()
